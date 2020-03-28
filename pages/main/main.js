7f54263e4608a7017c3a3585550e8f65e1c1e833
Zhiyuan Jiang <Jerry31415926535@gmail.com>
2020-03-29 09:32:50 +1000

    add user profile

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,17 +1,22 @@
 // pages/main.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.setData({
+      userInfo: app.globalData.userInfo
+    })
     wx.hideLoading({
       complete: (res) => {
         wx.showToast({
